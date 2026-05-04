d827fc2d728515fb3859a8fe38d980b9fc9fb7bb
blastonyz <blastonyzamora@gmail.com>
2026-05-04 11:44:00 -0300

    fix2

--- daoservice/next.config.ts
+++ daoservice/next.config.ts
@@ -2,6 +2,10 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   serverExternalPackages: ['pino', 'pino-pretty', 'thread-stream'],
+  webpack: (config) => {
+    config.externals.push('pino-pretty', 'encoding', 'thread-stream');
+    return config;
+  },
 };
 
 export default nextConfig;
